4040794c8ee75594cc4aa3b56262e579eab7db84
Janez T <hey@dz0ny.dev>
2025-04-01 11:07:31 +0200

    feat: implement notification system and replace legacy notify functions

--- notify/notify_linux.go
+++ notify/notify_linux.go
@@ -1,31 +1,10 @@
-package cmd
+package notify
 
 import (
 	"github.com/godbus/dbus/v5"
 )
 
-func Notify(message string) error {
-	conn, err := dbus.SessionBus()
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
-	call := obj.Call("org.freedesktop.Notifications.Notify", 0,
-		"ParetoSecurity",         // Application name
-		uint32(0),                // Replace ID
-		"",                       // Icon (empty for default)
-		"Pareto Linux",           // Summary
-		message,                  // Body
-		[]string{},               // Actions
-		map[string]interface{}{}, // Hints
-		int32(5000))              // Timeout (5 seconds)
-
-	return call.Err
-}
-
-func NotifyBlocking(message string) (string, error) {
+func Blocking(message string) (string, error) {
 	conn, err := dbus.SessionBus()
 	if err != nil {
 		return "", err
