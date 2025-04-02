9ecc0ddb653b1f4ef833d92423478002e094ab01
Janez T <hey@dz0ny.dev>
2025-04-02 10:36:07 +0200

    fix: prevent NixOS detection during testing by returning false in IsNixOS

--- shared/nixos.go
+++ shared/nixos.go
@@ -1,6 +1,9 @@
 package shared
 
-import "sync"
+import (
+	"sync"
+	"testing"
+)
 
 var isNixOSOnce sync.Once
 var isNixOS bool
@@ -9,6 +12,9 @@ var isNixOS bool
 // `nixos-version` command. It returns true if the command executes without
 // error, indicating that NixOS is likely the operating system.
 func IsNixOS() bool {
+	if testing.Testing() {
+		return false
+	}
 	isNixOSOnce.Do(func() {
 		_, err := RunCommand("nixos-version")
 		isNixOS = err == nil
