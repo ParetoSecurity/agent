c9fd55791ed1fb7c7191dd619cb969df29f3584a
Janez T <hey@dz0ny.dev>
2025-04-07 10:41:10 +0200

    fix: update comment for vendorHash to clarify update process

--- package.nix
+++ package.nix
@@ -8,7 +8,7 @@ in
     src = ./.;
     version = "${builtins.hashFile "sha256" "${toString ./go.sum}"}";
 
-    # Update this when Go dependencies change
+    # Updated with pre-commit, don't change manually
     vendorHash = "sha256-7mKAFkKGpBOjXc3J/sfF3k3pJF53tFybXZgbfJInuSY=";
 
     # Uncomment this while developing to skip Go tests
