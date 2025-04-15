0adaea918951539fe2ea2d0a6ea4996a672b3f8b
Janez T <hey@dz0ny.dev>
2025-04-15 20:32:59 +0200

    Enhance Windows OS version retrieval in CurrentReportingDevice and update OSVersion comment in ReportingDevice struct

--- shared/device_windows.go
+++ shared/device_windows.go
@@ -7,7 +7,7 @@ type ReportingDevice struct {
 	MachineUUID string `json:"machineUUID"` // e.g. 123e4567-e89b-12d3-a456-426614174000
 	MachineName string `json:"machineName"` // e.g. MacBook-Pro.local
 	Auth        string `json:"auth"`
-	OSVersion   string `json:"windowOSVersion"` // e.g. Ubuntu 20.04
+	OSVersion   string `json:"windowOSVersion"` // e.g. Windos 10 Pro (24H2)
 	ModelName   string `json:"modelName"`       // e.g. MacBook Pro
 	ModelSerial string `json:"modelSerial"`     // e.g. C02C1234
 }
