05c9a8244dfeb6167c579dd8be2457ae01d73214
Janez T <hey@dz0ny.dev>
2025-04-21 14:54:46 +0200

    fix: remove URI handler during uninstallation process

--- cmd/paretosecurity-installer/uninstall.ps1
+++ cmd/paretosecurity-installer/uninstall.ps1
@@ -12,6 +12,10 @@ Remove-Item -Recurse -Force -Path $InstallPath -ErrorAction SilentlyContinue
 # Remove uninstaller registry entry
 Remove-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ParetoSecurity" -Force -ErrorAction SilentlyContinue
 
+# Remove URI handler
+$URLHandlerKey = "HKCU:\Software\Classes\paretosecurity"
+Remove-Item -Path $URLHandlerKey -Recurse -Force -ErrorAction SilentlyContinue
+
 # Remove desktop shortcut
 $DesktopDir = [Environment]::GetFolderPath("Desktop")
 $DesktopShortcut = Join-Path $DesktopDir "Pareto Security.lnk"
