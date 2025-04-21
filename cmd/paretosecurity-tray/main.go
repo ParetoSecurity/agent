0aabf15a9bff4916469bf78398652f8bd1b872b6
Janez T <hey@dz0ny.dev>
2025-04-21 20:59:44 +0200

    fix: remove scheduled tasks for hourly updates and checks from main application

--- cmd/paretosecurity-tray/main.go
+++ cmd/paretosecurity-tray/main.go
@@ -2,7 +2,11 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"fyne.io/systray"
+	"github.com/ParetoSecurity/agent/shared"
 	"github.com/ParetoSecurity/agent/trayapp"
 	"github.com/caarlos0/log"
 )
@@ -11,6 +15,26 @@ func main() {
 	onExit := func() {
 		log.Info("Exiting...")
 	}
+
+	go func() {
+		for {
+			_, err := shared.RunCommand(shared.SelfExe(), "update")
+			if err != nil {
+				log.WithError(err).Error("Failed to run update command")
+			}
+			time.Sleep(time.Duration(50+rand.Intn(15)) * time.Minute)
+		}
+	}()
+
+	go func() {
+		for {
+			_, err := shared.RunCommand(shared.SelfExe(), "check")
+			if err != nil {
+				log.WithError(err).Error("Failed to run check command")
+			}
+			time.Sleep(time.Duration(45+rand.Intn(15)) * time.Minute)
+		}
+	}()
 
 	systray.Run(trayapp.OnReady, onExit)
 }
