f16b6e73d1f67aa8671b1f913e3b485c8c432e82
Janez Troha <239513+dz0ny@users.noreply.github.com>
2025-05-08 16:58:07 +0200

    Fix/installer-fixes-for-non-admin-user (#235)

--- shared/uninstall.ps1
+++ shared/uninstall.ps1
@@ -1,8 +1,17 @@
 $ErrorActionPreference = "Stop"
 
-# Stop the ParetoSecurity process
-Stop-Process -Name "paretosecurity-tray" -Force -ErrorAction SilentlyContinue
-Stop-Process -Name "paretosecurity" -Force -ErrorAction SilentlyContinue
+# Close running instances of ParetoSecurity applications
+Write-Host "Closing running instances of ParetoSecurity..."
+$procNames = @("paretosecurity-tray.exe", "paretosecurity.exe", "paretosecurity-tray", "paretosecurity")
+foreach ($name in $procNames) {
+    do {
+        $procs = Get-Process -Name $name -ErrorAction SilentlyContinue
+        if ($procs) {
+            $procs | Stop-Process -Force -ErrorAction SilentlyContinue
+            Start-Sleep -Milliseconds 300
+        }
+    } while ($procs)
+}
 
 # Remove installation directory
 $RoamingDir = [Environment]::GetFolderPath("ApplicationData")
@@ -10,7 +19,7 @@ $InstallPath = Join-Path $RoamingDir "ParetoSecurity"
 Remove-Item -Recurse -Force -Path $InstallPath -ErrorAction SilentlyContinue
 
 # Remove uninstaller registry entry
-Remove-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ParetoSecurity" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ParetoSecurity" -Force -ErrorAction SilentlyContinue
 
 # Remove URI handler
 $URLHandlerKey = "HKCU:\Software\Classes\paretosecurity"
@@ -25,7 +34,3 @@ Remove-Item -Path $DesktopShortcut -Force -ErrorAction SilentlyContinue
 $StartupDir = [Environment]::GetFolderPath("Startup")
 $StartupShortcut = Join-Path $StartupDir "Pareto Security.lnk"
 Remove-Item -Path $StartupShortcut -Force -ErrorAction SilentlyContinue
-
-# Remove scheduled task
-Unregister-ScheduledTask -TaskName "ParetoSecurityUpdate" -Confirm:$false -ErrorAction SilentlyContinue
-Unregister-ScheduledTask -TaskName "ParetoSecurityCheck" -Confirm:$false -ErrorAction SilentlyContinue
