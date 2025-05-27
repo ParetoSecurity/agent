bae73f17a768a92adb57042c77d57d56e1519df1
Neyts Zupan <zupo@users.noreply.github.com>
2025-05-27 09:34:28 +0100

    start socket after install

--- apt/postinstall.sh
+++ apt/postinstall.sh
@@ -6,5 +6,5 @@ if command -v systemctl >/dev/null 2>&1; then
     # Reload systemd and enable socket
     systemctl daemon-reload
     systemctl enable paretosecurity.service
-    systemctl enable paretosecurity.socket
+    systemctl enable --now paretosecurity.socket
 fi
