37792db08a10a28e8541a853c5d8c56ee6f94ee1
Janez T <hey@dz0ny.dev>
2025-07-04 15:00:36 +0200

    refactor: simplify main function in tray applications

--- cmd/paretosecurity-tray/main.go
+++ cmd/paretosecurity-tray/main.go
@@ -3,14 +3,7 @@
 
 package main
 
-import ()
-
 func main() {
-	onExit := func() {
-		log.Info("Exiting...")
-		os.Exit(0)
-	}
-	log.Info("Starting system tray application...")
-	trayApp := trayapp.NewTrayApp()
-	systray.Run(trayApp.OnReady, onExit)
+	app := NewTrayApp(nil)
+	app.Run()
 }
--- cmd/paretosecurity/main.go
+++ cmd/paretosecurity/main.go
@@ -1,8 +1,6 @@
 // Package main provides the entry point for the application.
 package main
 
-import ()
-
 func main() {
 	app := NewApp(nil)
 	app.Run()
