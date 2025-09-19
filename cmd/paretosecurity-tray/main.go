aa98d03174d2d43bb6b58d31aec80c000d3d9ed5
Janez T <hey@dz0ny.dev>
2025-09-19 15:05:13 +0200

    feat: implement simple instance locking mechanism to prevent multiple instances of the application
    ref: https://github.com/teamniteo/pareto/issues/809

--- cmd/paretosecurity-tray/main.go
+++ cmd/paretosecurity-tray/main.go
@@ -4,17 +4,21 @@
 package main
 
 import (
-	"github.com/allan-simon/go-singleinstance"
+	"os"
+	"path/filepath"
+
+	"github.com/ParetoSecurity/agent/shared"
 	"github.com/caarlos0/log"
 )
 
 func main() {
-	lockFile, err := singleinstance.CreateLockFile("paretosecurity-tray.lock")
-	if err != nil {
+
+	lockDir, _ := shared.UserHomeDir()
+	if err := shared.OnlyInstance(filepath.Join(lockDir, ".paretosecurity-tray.lock")); err != nil {
 		log.WithError(err).Fatal("An instance of ParetoSecurity tray application is already running.")
+		os.Exit(1) // just in case Fatal doesn't
 		return
 	}
-	defer lockFile.Close()
 
 	app := NewTrayApp(nil)
 	app.Run()
